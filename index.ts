4cff3d79f5f3aea013c0dba76bc64088289d9208
brk5673 <97960640+brk5673@users.noreply.github.com>
2025-01-16 21:55:34 -0300

    server set

--- index.ts
+++ index.ts
@@ -1,14 +1,30 @@
 import { Telegraf } from 'telegraf';
 import { setupCommands } from './commands';
+import express from 'express';
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN!);
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bot.webhookCallback('/bot'));
+bot.telegram.setWebhook(`${process.env.VERCEL_URL}/bot`);
+
+
 setupCommands(bot);
 
-bot.launch();
-console.log("Welcome to G7's bot on Telegram!");
+app.get('/', (req: any, res: any) => {
+  res.send('bot running');
+});
 
-bot.telegram.setMyCommands([
-  { command: 'start', description: 'Iniciar el bot' },
-]);
+app.listen(PORT, () => {
+  console.log(`bot running on port ${PORT}`);
+});
+
+// bot.launch();
+// console.log("Welcome to G7's bot on Telegram!");
+
+// bot.telegram.setMyCommands([
+//   { command: 'start', description: 'Iniciar el bot' },
+// ]);
